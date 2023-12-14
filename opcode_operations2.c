07f6b2feb97c42e4e8eace724c70a0f3b94187aa
kirlos basta <kirlosbasta@gmail.com>
2023-12-14 19:20:41 +0200

    Add division

--- opcode_operations2.c
+++ opcode_operations2.c
@@ -50,3 +50,33 @@ void sub(stack_t **stack, unsigned int line_number)
 	(*stack)->next->n -= (*stack)->n;
 	pop(stack, line_number);
 }
+
+/**
+ * division - divide the two element from top of the stack
+ * @stack: Front of the stack
+ * @line_number: line number
+ *
+ * Return: Nothing
+ */
+
+void division(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		free_dlistint();
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n != 0)
+	{
+		(*stack)->next->n /= (*stack)->n;
+	}
+	else
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		free_dlistint();
+		exit(EXIT_FAILURE);
+	}
+	pop(stack, line_number);
+}
+
